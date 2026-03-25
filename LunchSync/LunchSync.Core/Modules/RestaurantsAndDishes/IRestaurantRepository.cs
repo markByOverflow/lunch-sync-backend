f5ad51017c2d382932749d091bb36478e761d5fb
Phan Thanh Thao <phanthanhthao.21082005@gmail.com>
2026-03-25 17:05:59 +0700

    feat(restaurantrepository): implement IRestaurantRepository, RestaurantRepository

---
+++ LunchSync/LunchSync.Core/Modules/RestaurantsAndDishes/IRestaurantRepository.cs
@@ -0,0 +1,11 @@
+﻿using LunchSync.Core.Modules.RestaurantsAndDishes.Entities;
+namespace LunchSync.Core.Modules.RestaurantsAndDishes;
+
+public interface IRestaurantRepository
+{
+    // Lấy chi tiết 1 nhà hàng kèm theo danh sách món ăn (để map FeaturedDishes)
+    Task<Restaurant?> GetRestaurantWithDishesAsync(Guid id);
+
+    // Lấy danh sách nhà hàng theo danh sách ID (dùng cho logic xử lý sau voting)
+    Task<IEnumerable<Restaurant>> GetRestaurantsByIdsAsync(IEnumerable<Guid> ids);
+}
