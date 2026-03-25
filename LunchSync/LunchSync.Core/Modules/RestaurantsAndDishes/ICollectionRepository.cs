a942f6e2baf833afe9def9a54b0dd5de43f02480
Phan Thanh Thao <phanthanhthao.21082005@gmail.com>
2026-03-25 17:26:13 +0700

    feat(collectionrepository): implement ICollectionRepository, CollectionRepository

---
+++ LunchSync/LunchSync.Core/Modules/RestaurantsAndDishes/ICollectionRepository.cs
@@ -0,0 +1,9 @@
+﻿using LunchSync.Core.Modules.RestaurantsAndDishes.Entities;
+
+namespace LunchSync.Core.Modules.RestaurantsAndDishes;
+
+public interface ICollectionRepository
+{
+    Task<IEnumerable<Collection>> GetAllActiveCollectionsAsync();
+    Task<Collection?> GetCollectionByIdAsync(Guid id);
+}
