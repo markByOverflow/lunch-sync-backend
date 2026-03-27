0609c46eb3a01472f4668563ad993fcf0fd4b2cf
PaiMon26 <tranthiminhanh26102005@gmail.com>
2026-03-27 11:33:47 +0700

    hotfix: pin syntax

--- LunchSync/LunchSync.Core/Common/ValueObjects/Pin.cs
+++ LunchSync/LunchSync.Core/Common/ValueObjects/Pin.cs
@@ -1,4 +1,4 @@
-﻿using LunchSync.Core.Exceptions;
+using LunchSync.Core.Exceptions;
 using System.Security.Cryptography;
 
 namespace LunchSync.Core.Common.ValueObjects
@@ -12,22 +12,25 @@ namespace LunchSync.Core.Common.ValueObjects
             Value = value;
         }
 
-    public static Pin Generate()
-    {
-        string pinValue = RandomNumberGenerator.GetInt32(0, 1_000_000).ToString("D6");
-        return new Pin(pinValue);
-    }
+        // Tao ma PIN 6 chu so de dung cho flow session.
+        public static Pin Generate()
+        {
+            string pinValue = RandomNumberGenerator.GetInt32(0, 1_000_000).ToString("D6");
+            return new Pin(pinValue);
+        }
 
-    public override string ToString() => Value;
-    public override bool Equals(object? obj) => obj is Pin other && Value == other.Value;
-    public override int GetHashCode() => Value.GetHashCode();
+        public override string ToString() => Value;
+        public override bool Equals(object? obj) => obj is Pin other && Value == other.Value;
+        public override int GetHashCode() => Value.GetHashCode();
+
         public static Pin Create(string value)
         {
             if (string.IsNullOrWhiteSpace(value) || value.Length != 6 || !value.All(char.IsDigit))
             {
                 throw new BusinessRuleViolationException("PIN must be exactly 6 digits.");
             }
+
             return new Pin(value);
         }
-}
+    }
 }
