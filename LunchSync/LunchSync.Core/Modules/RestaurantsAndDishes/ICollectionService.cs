f4f9fe54ab925bb0c6f7df27ab4b65c52ab0315c
Phan Thanh Thao <phanthanhthao.21082005@gmail.com>
2026-03-29 18:17:49 +0700

    refactor(collection): modify return type and param

--- LunchSync/LunchSync.Core/Modules/RestaurantsAndDishes/ICollectionService.cs
+++ LunchSync/LunchSync.Core/Modules/RestaurantsAndDishes/ICollectionService.cs
@@ -5,8 +5,8 @@ namespace LunchSync.Core.Modules.RestaurantsAndDishes;
 public interface ICollectionService
 {
     // GET /collections
-    Task<IEnumerable<CollectionSummaryRes>> GetAllActiveCollectionsAsync();
+    Task<List<CollectionSummaryRes>> GetAllActiveCollectionsAsync(CancellationToken ct = default);
 
     // GET /collections/{id}
-    Task<CollectionDetailRes?> GetCollectionDetailAsync(Guid id);
+    Task<CollectionDetailRes?> GetCollectionDetailAsync(Guid id, CancellationToken ct = default);
 }
